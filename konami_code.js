b5cbdfbed5dfc050dc8f03d4795f34075c041cd6
Charlotte Payne <cltpayne@gmail.com>
2018-02-25 09:49:49 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,10 +1,11 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let index = 0 
 
 function init() {
-let index = 0 
+
 let body = document.querySelector('body')
 body.addEventListener('keydown', function(e) {
-  const key = parseInt(e.which);
+  const key = parseInt(e.which || e.detail);
   if (code[index] === key) {
     index++;
     if (index === code.length) {
