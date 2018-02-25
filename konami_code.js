ef2eee20d8e01da49f204a0192533a2487076c1a
Charlotte Payne <cltpayne@gmail.com>
2018-02-25 09:49:05 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,7 @@ function init() {
 let index = 0 
 let body = document.querySelector('body')
 body.addEventListener('keydown', function(e) {
-  const key = parseInt(e.which || e.detail);
+  const key = parseInt(e.which|| e.detail);
   if (code[index] === key) {
     index++;
     if (index === code.length) {
@@ -13,25 +13,3 @@ body.addEventListener('keydown', function(e) {
   } else {
     index = 0}
   } ); }
-
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-
-function init() {
-  let index = 0;
-
-  document.body.addEventListener('keydown', function(e) {
-    const key = parseInt(e.which || e.detail);
-
-    if (code[index] === key) {
-      index++;
-
-      if (index === code.length) {
-        alert('Hurray!');
-
-        index = 0;
-      }
-    } else {
-      index = 0;
-    }
-  });
-}
